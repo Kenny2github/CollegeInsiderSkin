07843a9bc571a99f0b95a533d7a26696253787c5
Kenny2github <kenny2minecraft@gmail.com>
2021-06-27 21:18:27 +0100

    Wrong indent

--- CollegeInsider.skin.php
+++ CollegeInsider.skin.php
@@ -4,14 +4,14 @@ class SkinCollegeInsider extends SkinTemplate {
 	var $skinname = 'collegeinsider', $stylename = 'CollegeInsider',
 		$template = 'CollegeInsiderTemplate', $useHeadElement = true;
 
-		public function initPage( OutputPage $out ) {
-			parent::initPage( $out );
-			$out->addModuleStyles( [
-				'mediawiki.skinning.interface', 'skins.collegeinsider'
-			] );
-			$out->addMeta(
-				'viewport',
-				'user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height'
-			);
-		}
+	public function initPage( OutputPage $out ) {
+		parent::initPage( $out );
+		$out->addModuleStyles( [
+			'mediawiki.skinning.interface', 'skins.collegeinsider'
+		] );
+		$out->addMeta(
+			'viewport',
+			'user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height'
+		);
+	}
 }
